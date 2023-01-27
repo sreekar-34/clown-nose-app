f56fa43977df6e6e5d117d75bbedd1ce3b47317a
sreekar-34 <95081023+sreekar-34@users.noreply.github.com>
2023-01-27 17:30:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,8 +7,26 @@ function setup(){
     video= createCapture(VIDEO);
     video.size(300,300);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw(){
     image(video, 0, 0, 300, 300);
+}
+
+function take_snapshot(){
+    save('MyFilterPhoto.png');
+}
+
+function modelLoaded(){
+    console.log("modelLoaded!");
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        console.log('nose x:'+results[0].pose.nose.x);
+        console.log('nose y:'+results[0].pose.nose.y);
+    }
 }
